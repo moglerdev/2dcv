c36af34d69db28064e173b93d3da83b1e958eaa6
Christopher Mogler <christopher@mogler.dev>
2025-05-05 16:29:03 +0200

    Add Sobel filter implementation and related files for image processing

--- 4.Aufgabe/sobel.py
+++ 4.Aufgabe/sobel.py
@@ -22,19 +22,42 @@ gray = rgb_2_gray(img)
 
 height, width = gray.shape
 
-# TODO: define filter in x in y direction
+# define filter in x in y direction
+filter_x = np.array([[1, 0, -1],
+                       [2, 0, -2],
+                       [1, 0, -1]])
+
+filter_y = np.array([[1, 2, 1],
+                     [0, 0, 0],
+                     [-1, -2, -1]])
+
 
 start = time.time()
+x_img = np.zeros((height-2, width-2))
 # TODO: filter image in x direction (sobel(gray, filter_x))
+for i in range(0, height-2):
+    for j in range(0, width-2):
+        for k in range(0, 3):
+            for l in range(0, 3):
+                x_img[i][j] += gray[i+k][j+l] * filter_x[k][l]
 end = time.time()
 duration = end-start
 print("Duration in milliseconds: ", duration*1000)
 
 start = time.time()
+y_img = np.zeros((height-2, width-2))
 # TODO: filter image in y direction (sobel(gray, filter_y))
+for i in range(0, height-2):
+    for j in range(0, width-2):
+        for k in range(0, 3):
+            for l in range(0, 3):
+                y_img[i][j] += gray[i+k][j+l] * filter_y[k][l]
 end = time.time()
 duration = end-start
 print("Duration in milliseconds: ", duration*1000)
 
+#save filtered image
+plt.imsave("sobel_x.jpg", x_img, cmap='gray')
+plt.imsave("sobel_y.jpg", y_img, cmap='gray')
 
 # TODO compute Gradient magnitude
